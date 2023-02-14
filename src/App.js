4e6f4a069436afad8d15acb7f024b8e27ac71e67
adeee11 <hello@iwebcode.design>
2023-02-14 18:37:10 +0530

    added automatic search on redirect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 import LoadingSpinner from "./LoadingSpinner";
 
-function App() {
+function App(props) {
   const [searchTerm, setSearchTerm] = useState("");
   const [showTitle, setShowTitle] = useState("");
   const [data, setData] = useState(null);
+  const [search, setSearch] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const params = new URLSearchParams(window.location.search);
 
   const handleSubmit = () => {
     if (searchTerm.trim().length === 0) {
@@ -23,10 +25,22 @@ function App() {
         setIsLoading(false);
         setShowTitle(response.data.organic_results);
         setData(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+        setSearch(false);
       });
   };
 
-  console.log(data, "data");
+  useEffect(() => {
+    if (search) handleSubmit();
+  }, [search]);
+
+  useEffect(() => {
+    const searchQuery = params.get("search");
+    if (searchTerm !== searchQuery) setSearchTerm(searchQuery);
+    setSearch(true);
+  }, []);
 
   const renderUser = (
     <div className="userlist-container">
@@ -58,7 +72,15 @@ function App() {
   return (
     <div className="App">
       <header className="">
-        <div style={{ display: "flex", justifyContent: "center" }}>
+        <form
+          style={{ display: "flex", justifyContent: "center" }}
+          onSubmit={(e) => e.preventDefault()}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && searchTerm.trim().length !== 0) {
+              handleSubmit();
+            }
+          }}
+        >
           <input
             placeholder="Search"
             style={{
@@ -72,13 +94,14 @@ function App() {
             onChange={(e) => setSearchTerm(e.target.value)}
           />
           <buttton
+            type="submit"
             className="srch-btn"
             onClick={handleSubmit}
             disabled={isLoading}
           >
             Submit{" "}
           </buttton>
-        </div>
+        </form>
         {isLoading ? <LoadingSpinner /> : renderUser}
       </header>
     </div>
